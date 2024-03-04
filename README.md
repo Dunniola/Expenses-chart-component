f74e6b072351c523d33607b266a52fe701460917
Dunniola <oladunniedu611@gmail.com>
2024-03-04 22:22:02 +0100

    Added new file

---
+++ README.md
@@ -0,0 +1,57 @@
+# Frontend Mentor - Expenses chart component solution
+
+This is a solution to the [Expenses chart component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/expenses-chart-component-e7yJBUdjwt). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the bar chart and hover over the individual bars to see the correct amounts for each day
+- See the current day’s bar highlighted in a different colour to the other bars
+- View the optimal layout for the content depending on their device’s screen size
+- See hover states for all interactive elements on the page
+
+
+### Screenshot
+Active states
+![](./design/active-states.jpg)
+Desktop design
+![](./design/desktop-design.jpg)
+Desktop preview
+![](./design/desktop-preview.jpg)
+Mobile design
+![](./design/mobile-design.jpg)
+
+
+
+- Solution URL: [View Demo](https://github.com/Dunniola/Expenses-chart-component.git)
+- Live Site URL: [Dunniola]()
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Author
+
+- Website - [Dunniola]()
+- Frontend Mentor - [@Dunniola](https://www.frontendmentor.io/profile/Dunniola)
+- Twitter - [@Eduoladunni](https://www.twitter.com/Eduoladunni)
---
+++ script.js
@@ -0,0 +1,82 @@
+const amountOne = document.querySelector('.amount-1');
+const amountTwo = document.querySelector('.amount-2');
+const amountThree = document.querySelector('.amount-3');
+const amountFour = document.querySelector('.amount-4');
+const amountFive = document.querySelector('.amount-5');
+const amountSix = document.querySelector('.amount-6');
+const amountSeven = document.querySelector('.amount-7');
+const hiddenElements = document.querySelectorAll('.hidden');
+
+document.querySelector(".bar-1").addEventListener('mouseover', (e) => {
+   amountOne.style.visibility = 'visible'
+  
+});
+document.querySelector(".bar-1").addEventListener('mouseout', (e) => {
+    amountOne.style.visibility = 'hidden'
+   
+ });
+
+ document.querySelector(".bar-1").addEventListener('mouseover', (e) => {
+    amountOne.style.visibility = 'visible'
+   
+ });
+ document.querySelector(".bar-1").addEventListener('mouseout', (e) => {
+     amountOne.style.visibility = 'hidden'
+    
+  });
+
+  document.querySelector(".bar-2").addEventListener('mouseover', (e) => {
+    amountTwo.style.visibility = 'visible'
+   
+ });
+ document.querySelector(".bar-2").addEventListener('mouseout', (e) => {
+     amountTwo.style.visibility = 'hidden'
+    
+  });
+
+  document.querySelector(".bar-3").addEventListener('mouseover', (e) => {
+    amountThree.style.visibility = 'visible'
+   
+ });
+ document.querySelector(".bar-3").addEventListener('mouseout', (e) => {
+     amountThree.style.visibility = 'hidden'
+    
+  });
+
+  document.querySelector(".bar-5").addEventListener('mouseover', (e) => {
+    amountFive.style.visibility = 'visible'
+   
+ });
+ document.querySelector(".bar-5").addEventListener('mouseout', (e) => {
+     amountFive.style.visibility = 'hidden'
+    
+  });
+ 
+  document.querySelector(".bar-4").addEventListener('mouseover', (e) => {
+    amountFour.style.visibility = 'visible'
+   
+ });
+ document.querySelector(".bar-4").addEventListener('mouseout', (e) => {
+     amountFour.style.visibility = 'hidden'
+    
+  });
+
+  
+  document.querySelector(".bar-6").addEventListener('mouseover', (e) => {
+    amountSix.style.visibility = 'visible'
+   
+ });
+ document.querySelector(".bar-6").addEventListener('mouseout', (e) => {
+     amountSix.style.visibility = 'hidden'
+    
+  });
+ 
+  document.querySelector(".bar-7").addEventListener('mouseover', (e) => {
+    amountSeven.style.visibility = 'visible'
+   
+ });
+ document.querySelector(".bar-7").addEventListener('mouseout', (e) => {
+     amountSeven.style.visibility = 'hidden'
+    
+  });
+ 
